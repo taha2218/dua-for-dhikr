9ebb4da648f0b91d9d61cf684159501f92aa3eaf
SHAHID-EXTERNAL Mohammad-Taha <S0137884@gemalto.com>
2025-04-11 13:13:27 +0530

    Safe Area Fix

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,9 +1,21 @@
 import './LoginPage.css';
+import { useEffect } from 'react';
 import duaLogo from '../../static/Logos/DUA_LOGO.png';
 import Illustration from '../../static/Logos/mosque.png';
 import googleIcon from '../../static/Logos/search.png';
 
 function LoginPage() {
+    useEffect(() => {
+        const setViewportHeight = () => {
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        };
+
+        setViewportHeight();
+        window.addEventListener('resize', setViewportHeight);
+        return () => window.removeEventListener('resize', setViewportHeight);
+    }, []);
+
     return (
         <div className='login-page-container'>
             <div className='dua-illustration'>
