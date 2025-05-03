29389858ee95b9174b42999d6d6efbcd2ea313c9
SHAHID-EXTERNAL Mohammad-Taha <S0137884@gemalto.com>
2025-05-03 11:02:23 +0530

    Added hadith with ref

--- src/Pages/DuaDetailPage/DuaDetailPage.js
+++ src/Pages/DuaDetailPage/DuaDetailPage.js
@@ -1,11 +1,10 @@
 import { useParams } from "react-router-dom";
 import Taskbar from "../../Common/Taskbar/Taskbar";
 import dhikrList from "../../Data/Dhikr.json";
-import './DuaDetailPage.css'; 
+import './DuaDetailPage.css';
 
 const DuaDetailPage = () => {
     const { id } = useParams();
-
     const selectedDua = dhikrList.find(item => item.id === id);
 
     if (!selectedDua) {
@@ -23,6 +22,17 @@ const DuaDetailPage = () => {
                     {selectedDua.arabic}
                 </div>
             </div>
+
+            {selectedDua.references && selectedDua.references.length > 0 && (
+                <div className="reference-section">
+                    {selectedDua.references.map((ref, index) => (
+                        <div className="reference-block" key={index}>
+                            <p className="reference-text">{ref.text}</p>
+                            <p className="reference-source">{ref.source}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
